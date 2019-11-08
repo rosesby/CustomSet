d23aeee1b6ac188f63f1a047091d41064538cdc6
saula <saulalonsopalazuelos@outlook.com>
2019-11-08 14:55:57 -0700

    a2

--- src/com/mcc/CustomSet.java
+++ src/com/mcc/CustomSet.java
@@ -1,4 +1,62 @@
 package com.mcc;
 
-public class CustomSet extends AbstractSet<T> {
+import java.util.AbstractSet;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class CustomSet<T> extends AbstractSet<T> {
+
+    private T[] array;
+
+    public CustomSet(T[] array) {
+        this.array = array;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new CustomSetIterator();
+    }
+
+    // Return the length of the array
+    @Override
+    public int size() {
+        return array.length;
+    }
+
+    // Set the value at a given index
+    public void set(int index, T value) {
+        array[index] = value;
+    }
+
+    // Return the value at a given index
+    public T get(int index) {
+        return array[index];
+    }
+
+    private class CustomSetIterator implements Iterator<T>{
+        // the current element we are looking at
+        int current = 0;
+
+        // return whether or not there are more elements in the array that
+        // have not been iterated over.
+        @Override
+        public boolean hasNext() {
+            if (current < array.length) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        // return the next element of the iteration and move the current
+        // index to the element after that.
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return array[current++];
+        }
+    }
 }
--- src/com/mcc/Main.java
+++ src/com/mcc/Main.java
@@ -4,5 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        // create an array of strings
+        String[] strings = new String[]{"Hello", "World"};
+
+        // create a new array to hold these strings
+        CustomSet<String> array = new CustomSet<String>(strings);
+
+        // get and print the first values (prints "Hello")
+        System.out.println(array.get(0));
+
+        // set the second value
+        array.set(1, "Javaland!");
+
+        // iterate over the array, printing "Hello\nJavaland!"
+        for (String s : array) {
+            System.out.println(s);
+        }
+
     }
 }
